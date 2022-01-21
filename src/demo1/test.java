41dd755578e31f694bebb34cd719b2ef42772133
mahir999 <mahirjeet@gmail.com>
2022-01-21 15:31:10 +0600

    Pushing democode to subranch

--- src/demo1/test.java
+++ src/demo1/test.java
@@ -3,6 +3,6 @@ package demo1;
 public class test {
   public static void main(String args[])
   {
-	  System.out.println("Hello World How Are You");
+	 System.out.println("Hello World How Are You");
   }
 }
